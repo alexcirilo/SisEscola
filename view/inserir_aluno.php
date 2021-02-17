21b371e37e15480674ce024d8bec379b39fb8a79
alexcirilo <alexcirilo@github.com>
2021-02-17 19:46:34 -0300

    inserir aluno

--- view/inserir_aluno.php
+++ view/inserir_aluno.php
@@ -6,10 +6,10 @@ if (!isset($_GET['editar'])) { ?>
 
     <form action="processa_aluno.php" method="POST">
         <label>Nome:</label>
-        <input type="text" name="nome_aluno" placeholder="Insira o nome do Aluno">
+        <input class="form-control" type="text" name="nome_aluno" placeholder="Insira o nome do Aluno">
         <br>
         <label>Data Nascimento:</label>
-        <input type="text" name="data_nascimento" placeholder="dd/MM/yyyy" class="form-control" data-mask="00-00-0000">;
+        <input type="text" name="data_nascimento" placeholder="dd/MM/yyyy" class="form-control" data-mask="00-00-0000">
         <div>
             <hr /><br>
             <input type="submit" value="salvar">
